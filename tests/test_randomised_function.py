0f0fe78dabcce583d1ccc9cdddff7ee5520ccbeb
Liam Dormon <liamdormon04@gmail.com>
2026-03-16 12:01:53 +0000

    fix flaky randomised_function tests

--- tests/test_randomised_function.py
+++ tests/test_randomised_function.py
@@ -1,8 +1,17 @@
 import unittest
+from unittest.mock import patch
 from lecture import randomised_function
 
 class MyTestCase(unittest.TestCase):
 
-    def test_randomised_function(self):
-        self.assertEqual('software', randomised_function())  # This will pass or fail randomly
-        # TODO: Can we make this test deterministic? (HINT: Mock testing)
+    @patch('random.randint')
+    def test_randomised_function_low(self, mock_randint):
+        mock_randint.return_value = 3
+
+        self.assertEqual('software', randomised_function()) 
+
+    @patch('random.randint')
+    def test_randomised_function_high(self, mock_randint):
+        mock_randint.return_value = 7
+
+        self.assertEqual('engineering', randomised_function())
